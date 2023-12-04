11569b06872400cbbe9dcbfeb73a4327ab486cd1
Luka Mate Granic <luka.granic@microblink.com>
2023-12-04 11:26:24 +0100

    day04_00

---
+++ src/bin/day04_00/main.rs
@@ -0,0 +1,35 @@
+use std::collections::BTreeSet;
+
+fn to_set(s: &str) -> BTreeSet<i32> {
+    s.split_whitespace()
+        .map(str::parse)
+        .map(Result::unwrap)
+        .collect()
+}
+
+fn winning_score(s: String) -> Option<i32> {
+    let (_, interesting_part) = s.split_once(':')?;
+
+    let (my_numbers, winning_numbers) = interesting_part.split_once("|")?;
+
+    let my_numbers     : BTreeSet<i32> = to_set(my_numbers);
+    let winning_numbers: BTreeSet<i32> = to_set(winning_numbers);
+
+    let intersection = my_numbers.intersection(&winning_numbers);
+
+    match intersection.count() {
+        0 => None,
+        c => Some(1 << ( c - 1 ))
+    }
+}
+
+fn main () {
+    let sum: i32 = std::io::stdin()
+        .lines()
+        .map_while(Result::ok)
+        .map(winning_score)
+        .flatten()
+        .sum();
+
+    println!("{}", sum);
+}
