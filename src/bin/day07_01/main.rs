fb4fac2a62bc30547cbd7727cb695ef5bca07878
Luka Mate Granic <luka.granic@microblink.com>
2023-12-07 20:32:22 +0100

    day07_01

--- src/bin/day07_01/main.rs
+++ src/bin/day07_01/main.rs
@@ -1,33 +1,27 @@
+fn main() {
+    let mut hands_and_bids: Vec<(Hand, i64)> = std::io::stdin()
+        .lines()
+        .map_while(Result::ok)
+        .map(|s| {
+            let (hand, bid) = s.split_once(' ').unwrap();
+            (Hand::from(hand), bid.parse().unwrap())
+        })
+        .collect();
+
+    hands_and_bids.sort_by_key(|(h, _)| *h);
+
+    let total_score = hands_and_bids
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, (_, bid))| (i + 1) as i64 * *bid )
+        .sum::<i64>();
+
+    println!("{}", total_score);
+}
+
 #[derive(Ord, PartialOrd, PartialEq, Eq, Copy, Clone)]
-struct Card(u8); // 0..11
-
-impl Card {
-    const MAX_STRENGTH: Self = Self::from_char('A');
-
-    const fn from_char(c: char) -> Self {
-        match c {
-            '0'..='9' => Self(c as u8 - b'0'),
-            'T' => Self(10),
-            'J' => Self(11),
-            'Q' => Self(12),
-            'K' => Self(13),
-            'A' => Self(14),
-            _ => panic!(),
-        }
-    }
-
-    pub fn new(value: u8) -> Self {
-        assert!(value <= Self::MAX_STRENGTH.0);
-        Self(value)
-    }
-}
-
-impl From<char> for Card {
-    fn from(c: char) -> Self {
-        Self::from_char(c)
-    }
-}
-
+struct Card(u8);
 
 #[derive(Ord, PartialOrd, PartialEq, Eq, Copy, Clone)]
 enum HandStrength {
@@ -43,7 +37,34 @@ enum HandStrength {
 #[derive(Copy, Clone, Eq, PartialEq)]
 struct Hand {
     card_counts: [(Card, u8); 5],
-    sorted_cards: [Card; 5],
+    original_order: [Card; 5],
+}
+
+impl Card {
+    const MAX_STRENGTH: Self = Self::from_char('A');
+
+    const fn from_char(c: char) -> Self {
+        match c {
+            'J' => Self(0),
+            '2'..='9' => Self(c as u8 - b'2' + 1u8),
+            'T' => Self(9),
+            'Q' => Self(10),
+            'K' => Self(11),
+            'A' => Self(12),
+            _ => panic!(),
+        }
+    }
+
+    pub fn new(value: u8) -> Self {
+        assert!(value <= Self::MAX_STRENGTH.0);
+        Self(value)
+    }
+}
+
+impl From<char> for Card {
+    fn from(c: char) -> Self {
+        Self::from_char(c)
+    }
 }
 
 impl From<&str> for Hand {
@@ -61,13 +82,21 @@ impl From<&str> for Hand {
             card_counts[i as usize].0 = Card::new(i);
         }
 
+        let mut joker_count = 0;
+
         for card in sorted_cards.iter() {
-            card_counts[card.0 as usize].1 += 1;
+            if card.0 == 0 {
+                joker_count += 1;
+            } else {
+                card_counts[card.0 as usize].1 += 1;
+            }
         }
 
         card_counts.sort_by(|(_, a_count), (_, b_count)| b_count.cmp(a_count));
 
-        Self { card_counts: card_counts[0..5].try_into().unwrap(), sorted_cards }
+        card_counts[0].1 += joker_count;
+
+        Self { card_counts: card_counts[0..5].try_into().unwrap(), original_order: sorted_cards }
     }
 }
 
@@ -90,7 +119,7 @@ impl Hand {
         if hand_strength != std::cmp::Ordering::Equal {
             hand_strength
         } else {
-            other.sorted_cards.cmp(&self.sorted_cards)
+            other.original_order.cmp(&self.original_order)
         }
     }
 }
@@ -105,27 +134,4 @@ impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.compare_strength(other)
     }
-}
-
-
-fn main() {
-    let mut hands_and_bids: Vec<(Hand, i64)> = std::io::stdin()
-        .lines()
-        .map_while(Result::ok)
-        .map(|s| {
-            let (hand, bid) = s.split_once(' ').unwrap();
-            (Hand::from(hand), bid.parse().unwrap())
-        })
-        .collect();
-
-    hands_and_bids.sort_by_key(|(h, _)| *h);
-
-    hands_and_bids.reverse();
-
-    let total_score = hands_and_bids.iter()
-        .enumerate()
-        .map(|(i, (_, bid))| (i + 1) as i64 * *bid )
-        .sum::<i64>();
-
-    println!("{}", total_score);
 }
