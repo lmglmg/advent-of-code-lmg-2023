b585f9ad80d26c7603d669fe89f02e14bc8d256b
Luka Mate Granic <luka.granic@microblink.com>
2023-12-08 17:31:27 +0100

    tuning

--- src/bin/day08_01/main.rs
+++ src/bin/day08_01/main.rs
@@ -1,10 +1,11 @@
 use std::collections::BTreeMap;
+use num::integer::lcm;
 use advent_of_code_lmg_2023::*;
 
 fn main() {
     let mut lines = std::io::stdin().lines();
 
-    let decisions_root = lines.read_line().into_bytes();
+    let choices_base = lines.read_line().into_bytes();
 
     lines.skip_line();
 
@@ -22,8 +23,8 @@ fn main() {
 
     let full_cyclic_path = all_starting_locations
         .iter()
-        .map(|&starting_location| cyclic_path(&paths, starting_location, &decisions_root))
-        .fold(1, num::integer::lcm);
+        .map(|&starting_location| cyclic_path(&paths, starting_location, &choices_base))
+        .fold(1, lcm);
 
     println!("{}", full_cyclic_path);
 }
@@ -31,35 +32,36 @@ fn main() {
 fn cyclic_path(
     paths: &BTreeMap<LocationID, (LocationID, LocationID)>,
     starting_location: LocationID,
-    decisions_root: &[u8],
+    choices_base: &[u8],
 ) -> i64 {
-    let mut decision_root_it = decisions_root.iter();
+    let mut choices_base_it = choices_base.iter();
 
     let mut current_location = starting_location;
 
     let mut steps: i64 = 0;
 
     while !current_location.is_ending() {
-        let (l_choice, r_choice) = paths.get(&current_location).unwrap();
+        let (left_location, right_location) = paths.get(&current_location).unwrap();
 
-        let decision = match decision_root_it.next() {
+        let decision = match choices_base_it.next() {
             Some(choice) => choice,
             None => {
-                decision_root_it = decisions_root.iter();
-                decision_root_it.next().unwrap()
+                // Reset path
+                choices_base_it = choices_base.iter();
+                choices_base_it.next().unwrap()
             }
         };
 
-        match decision {
-            b'L' => current_location = *l_choice,
-            b'R' => current_location = *r_choice,
+        current_location = match decision {
+            b'L' => *left_location,
+            b'R' => *right_location,
             _ => unreachable!(),
-        }
+        };
 
         steps += 1;
     }
 
-    num::integer::lcm(steps, decisions_root.len() as i64)
+    lcm(steps, choices_base.len() as i64)
 }
 
 #[derive(PartialOrd, Ord, PartialEq, Eq, Copy, Clone)]
@@ -99,9 +101,6 @@ impl ParsedLine {
             .strip_suffix(')')?
             .split_once(", ")?;
 
-        let l_choice = LocationID::from(l_choice);
-        let r_choice = LocationID::from(r_choice);
-
-        Some(Self { location: target, l_choice, r_choice })
+        Some(Self{location: target, l_choice: l_choice.into(), r_choice: r_choice.into()})
     }
 }
