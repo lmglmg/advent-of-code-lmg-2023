dcbd1848fcc0cb345e2483a8bdda8b32abf8d677
Luka Mate Granic <luka.granic@microblink.com>
2023-12-11 22:37:01 +0100

    day11

---
+++ src/bin/day11_01/main.rs
@@ -0,0 +1,52 @@
+use advent_of_code_lmg_2023::read_matrix_from_input;
+
+fn main() {
+    let input = read_matrix_from_input();
+
+    let mut row_distance = vec![1000000i64; input.height() as usize];
+    let mut col_distance = vec![1000000i64; input.width () as usize];
+
+    let mut element_indices: Vec<(i64, i64)> = Vec::new();
+
+    for (h, w, element) in input.iter(){
+        if element != b'.' {
+            row_distance[h as usize] = 1;
+            col_distance[w as usize] = 1;
+            element_indices.push((h, w));
+        }
+    }
+
+    let row_accumulated_distances: Vec<_> = row_distance
+        .iter()
+        .scan(0, |state, &x| {
+            *state += x;
+            Some(*state)
+        })
+        .collect();
+
+    let col_accumulated_distances: Vec<_> = col_distance
+        .iter()
+        .scan(0, |state, &x| {
+            *state += x;
+            Some(*state)
+        })
+        .collect();
+
+    let mut distance_sum = 0;
+
+    for i in 0..element_indices.len() {
+        for j in i+1..element_indices.len() {
+            let (h1, w1) = element_indices[i];
+            let (h2, w2) = element_indices[j];
+
+            let dist_h = (row_accumulated_distances[h2 as usize] - row_accumulated_distances[h1 as usize]).abs();
+            let dist_w = (col_accumulated_distances[w2 as usize] - col_accumulated_distances[w1 as usize]).abs();
+
+            let dist = dist_h + dist_w;
+
+            distance_sum += dist;
+        }
+    }
+
+    println!("{}", distance_sum);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,137 @@ impl StdinExt for Lines<StdinLock<'_>> {
     }
 
 }
+
+pub fn read_all_lines_as_bytes() -> Vec<Vec<u8>> {
+    let lines = std::io::stdin().lines();
+
+    lines
+        .map_while(Result::ok)
+        .map(String::into_bytes)
+        .collect::<Vec<_>>()
+}
+
+struct ByteMatrixBuilder {
+    width: Option<usize>,
+    data: Vec<u8>,
+}
+
+impl ByteMatrixBuilder {
+    pub fn new() -> Self {
+        Self {
+            width: None,
+            data: Vec::new(),
+        }
+    }
+
+    pub fn add_line(&mut self, line: &[u8]) {
+        if let Some(width) = self.width {
+            assert_eq!(width, line.len());
+        } else {
+            self.width = Some(line.len());
+
+            // Pre-allocate a square matrix
+            self.data.reserve(line.len() * line.len());
+        }
+
+        self.data.extend_from_slice(line);
+    }
+
+    pub fn build(self) -> ByteMatrix {
+        let width = self.width.unwrap();
+        let height = self.data.len() / width;
+
+        ByteMatrix {
+            data: self.data,
+            width,
+            height,
+        }
+    }
+}
+
+pub struct ByteMatrix {
+    data: Vec<u8>,
+    width: usize,
+    height: usize,
+}
+
+impl ByteMatrix {
+    pub fn at(&self, x: i64, y: i64) -> Option<u8> {
+        if x < 0 || y < 0 {
+            return None;
+        }
+        if x > self.width() || y > self.height() {
+            return None;
+        }
+        let index = (y * self.width() + x) as usize;
+        unsafe{ Some(*self.data.get_unchecked(index)) }
+    }
+
+    pub fn at_mut(&mut self, x: i64, y: i64) -> Option<&mut u8> {
+        if x < 0 || y < 0 {
+            return None;
+        }
+        if x > self.width() || y > self.height() {
+            return None;
+        }
+        let index = (y * self.width() + x) as usize;
+        unsafe{ Some(self.data.get_unchecked_mut(index)) }
+    }
+
+    pub fn width(&self) -> i64 {
+        self.width as i64
+    }
+
+
+    pub fn height(&self) -> i64 {
+        self.height as i64
+    }
+
+    pub fn iter(&self) -> ByteMatrixIter {
+        ByteMatrixIter {
+            matrix: self,
+            x: 0,
+            y: 0,
+            total_iterations: 0,
+        }
+    }
+}
+
+pub struct ByteMatrixIter<'a> {
+    matrix: &'a ByteMatrix,
+    x: i64,
+    y: i64,
+    total_iterations: i64,
+}
+
+impl<'a> Iterator for ByteMatrixIter<'a> {
+    type Item = (i64, i64, u8);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.total_iterations as usize >= self.matrix.data.len() {
+            return None;
+        }
+        let x = self.x;
+        let y = self.y;
+        let i = self.total_iterations;
+
+        self.x += 1;
+        if self.x >= self.matrix.width() {
+            self.x = 0;
+            self.y += 1;
+        }
+        self.total_iterations += 1;
+
+        unsafe{ Some((y, x, *self.matrix.data.get_unchecked(i as usize))) }
+    }
+}
+
+pub fn read_matrix_from_input() -> ByteMatrix {
+    let mut builder = ByteMatrixBuilder::new();
+
+    for line in std::io::stdin().lines() {
+        builder.add_line(&line.unwrap().into_bytes());
+    }
+
+    builder.build()
+}
